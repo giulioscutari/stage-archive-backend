b50f1e6e086521a08aa6dffd13ab245cbdd9177c
Giulio <giulio@20tab.com>
2022-11-02 10:30:08 +0000

    minor changes and requirements updated for testing

--- archive/test.sh
+++ archive/test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+pwd > $PWD
+cd /app/archive
 coverage run -m pytest
 coverage report
 coverage html
+echo "file:///home/"$(whoami)"/personal/stage-archive/stage-archive-backend/archive/htmlcov/index.html"
+cd $PWD
